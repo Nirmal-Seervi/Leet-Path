dbe7b6294e165d198ad043a822e8ab83d45c6d83
NimCraft <128449025+Nirmal-Seervi@users.noreply.github.com>
2024-10-28 03:16:11 +0530

    Time: 90 ms (16.44%), Space: 86.3 MB (35.01%) - LeetHub

---
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.java
@@ -0,0 +1,67 @@
+class Solution {
+
+    public int[] treeQueries(TreeNode root, int[] queries) {
+        Map<Integer, Integer> resultMap = new HashMap<>();
+        Map<TreeNode, Integer> heightCache = new HashMap<>();
+
+        dfs(root, 0, 0, resultMap, heightCache);
+
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            result[i] = resultMap.get(queries[i]);
+        }
+        return result;
+    }
+
+    
+    private int height(TreeNode node, Map<TreeNode, Integer> heightCache) {
+        if (node == null) {
+            return -1;
+        }
+
+        
+        if (heightCache.containsKey(node)) {
+            return heightCache.get(node);
+        }
+
+        int h =
+            1 +
+            Math.max(
+                height(node.left, heightCache),
+                height(node.right, heightCache)
+            );
+        heightCache.put(node, h);
+        return h;
+    }
+
+   
+    private void dfs(
+        TreeNode node,
+        int depth,
+        int maxVal,
+        Map<Integer, Integer> resultMap,
+        Map<TreeNode, Integer> heightCache
+    ) {
+        if (node == null) {
+            return;
+        }
+
+        resultMap.put(node.val, maxVal);
+
+      
+        dfs(
+            node.left,
+            depth + 1,
+            Math.max(maxVal, depth + 1 + height(node.right, heightCache)),
+            resultMap,
+            heightCache
+        );
+        dfs(
+            node.right,
+            depth + 1,
+            Math.max(maxVal, depth + 1 + height(node.left, heightCache)),
+            resultMap,
+            heightCache
+        );
+    }
+}
